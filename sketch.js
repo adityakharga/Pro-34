52d74b67611e59b034255015a9e558baf7970a06
adityakharga <72646575+adityakharga@users.noreply.github.com>
2021-03-14 16:25:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,7 +63,7 @@ function draw() {
 }
 
 function keyPressed() { 
-	if (keyCode === UP_ARROW) 
+	if (keyCode === "space") 
 	{ Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-100,y:-100}); 
 }
 } 
